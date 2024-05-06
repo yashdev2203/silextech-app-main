3b0bf36fa9a1cd13428d12ad3b77fd6ae494de88
YASH <92169629+YASH2221@users.noreply.github.com>
2024-05-06 22:47:15 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,180 @@
+import Link from 'next/link';
+import { type ReactElement, useState } from 'react';
+// import { useTranslation } from 'next-i18next';
+import type { NextPageWithLayout } from 'types';
+import { GetServerSidePropsContext } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import FAQSection from '@/components/defaultLanding/FAQSection';
+import HeroSection from '@/components/defaultLanding/HeroSection';
+import FeatureSection from '@/components/defaultLanding/FeatureSection';
+import PricingSection from '@/components/defaultLanding/PricingSection';
+// import useTheme from 'hooks/useTheme';
+import env from '@/lib/env';
+import Head from 'next/head';
+import { Dialog } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import FooterSection from '@/components/defaultLanding/FooterSection';
+import TestimonialSection from '@/components/defaultLanding/TestimonialSection';
+import ContactSection from '@/components/defaultLanding/ContactSection';
+
+const navigation = [
+  { name: 'Product', href: '/Features' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+];
+
+const Home: NextPageWithLayout = () => {
+  // const { toggleTheme, selectedTheme } = useTheme();
+  // const { t } = useTranslation('common');
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <Head>
+        <title>Silextech</title>
+      </Head>
+
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex lg:flex-1">
+            <Link href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                alt=""
+              />
+            </Link>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-white"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <Link
+              href="/auth/join"
+              className="text-sm font-semibold leading-6 text-white px-2"
+            >
+              Sign up
+            </Link>
+            <Link
+              href="/auth/login"
+              className="text-sm font-semibold leading-6 text-white px-2"
+            >
+              Log in
+            </Link>
+          </div>
+        </nav>
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
+          <div className="fixed inset-0 z-50" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
+                />
+              </a>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
+      <main>
+        <HeroSection />
+        <FeatureSection />
+        <PricingSection />
+        <TestimonialSection />
+        <FAQSection />
+        <ContactSection />
+        <FooterSection />
+      </main>
+    </>
+  );
+};
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  // Redirect to login page if landing page is disabled
+  if (env.hideLandingPage) {
+    return {
+      redirect: {
+        destination: '/auth/login',
+        permanent: true,
+      },
+    };
+  }
+
+  const { locale } = context;
+
+  return {
+    props: {
+      ...(locale ? await serverSideTranslations(locale, ['common']) : {}),
+    },
+  };
+};
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <>{page}</>;
+};
+
+export default Home;
