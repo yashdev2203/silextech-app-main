8a4e52435336d4c97cd556941cf0cbf5c6e81be9
yashdev2203 <yashcodewise@gmail.com>
2024-05-07 10:40:50 +0530

    update package.json and next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const { withSentryConfig } = require('@sentry/nextjs');
 const nextConfig = {
   experimental: { esmExternals: false, webpackBuildWorker: true },
   reactStrictMode: true,
+  output:'standalone',
   images: {
     remotePatterns: [
       {
